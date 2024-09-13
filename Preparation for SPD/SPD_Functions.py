abccbc299ae4cbaf19344d0a4f34df5b82001be4
Dongxu Liu <liudongxu145@gmail.com>
2024-09-13 15:02:00 +0800

    Update SPD_Functions.py

--- Preparation for SPD/SPD_Functions.py
+++ Preparation for SPD/SPD_Functions.py
@@ -1,1 +1,9 @@
-
+import scipy
+#using LogmTranfer, you can transform 3D data on SPD manifold into tangent space
+def LogmTranfer(myRM):
+    size1=len(myRM)
+    size2=len(myRM[1])
+    myRMlog=np.zeros((size1,size2,size2))
+    for i in range(len(myRM)):
+        myRMlog[i]=scipy.linalg.logm(myRM[i], disp=True)#
+    return myRMlog
