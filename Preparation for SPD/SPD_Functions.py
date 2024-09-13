dccbbd758d61a4c29681374ec85b16f9123e2baf
Dongxu Liu <liudongxu145@gmail.com>
2024-09-13 15:13:15 +0800

    Update SPD_Functions.py

--- Preparation for SPD/SPD_Functions.py
+++ Preparation for SPD/SPD_Functions.py
@@ -1,9 +1,23 @@
 import scipy
+
+#using RM_M_G, you can transform the time series EEG signal to SPD matrices
+#the shape 0f data (N, n, len)   N is the number of samples, n is channels of EEG data, len is the length of EEG data
+def RM_M_G(data):
+    size1=len(data)
+    size2=len(data[1])
+    RM_M=np.zeros((size1,size2,size2))
+    for i in range(size1):
+        tmp=data[i]
+        #1280 here is the len, you can change it base on the shape of your EEG data.
+        rmdata=np.dot(tmp,tmp.T)/(1280-1)+1e-9
+        RM_M[i]=rmdata
+    return RM_M
 #using LogmTranfer, you can transform 3D data on SPD manifold into tangent space
-def LogmTranfer(myRM):
-    size1=len(myRM)
-    size2=len(myRM[1])
-    myRMlog=np.zeros((size1,size2,size2))
-    for i in range(len(myRM)):
-        myRMlog[i]=scipy.linalg.logm(myRM[i], disp=True)#
+#the shape of input 3D data (N,d,d) N is the number of samples, d is the the size of SPD matrix
+def LogmTranfer(mySPDdata):
+    Dimension_1=len(mySPDdata)
+    Dimension_2=len(mySPDdata[1])
+    myRMlog=np.zeros((Dimension_1,Dimension_2,Dimension_2))
+    for i in range(len(mySPDdata)):
+        myRMlog[i]=scipy.linalg.logm(mySPDdata[i], disp=True)#
     return myRMlog
