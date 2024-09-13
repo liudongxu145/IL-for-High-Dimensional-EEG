514878787aa77dbb6f293207d30cc3d6e2e299e6
Dongxu Liu <liudongxu145@gmail.com>
2024-09-13 15:37:08 +0800

    Update DR.py

--- Dimensionality reduction methods/DR.py
+++ Dimensionality reduction methods/DR.py
@@ -25,20 +25,52 @@ x_train_R=x_train_R.reshape(-1,wantaftersize,wantaftersize)
 x_val_R=x_val_R.reshape(-1,wantaftersize,wantaftersize)
 
 #2DPCA
-x_train_R=PCA2Dfor3DimData(x_train,wantaftersize)
+def PCA2D_2D(samples, row_top, col_top):
+    '''samples are 3d matrices'''
+    size = samples[0].shape
+    # m*n matrix
+    mean = np.zeros(size)
+
+    for s in samples:
+        mean = mean + s
+    # get the mean of all samples
+    mean /= float(len(samples))
+
+    # n*n matrix
+    cov_row = np.zeros((size[1],size[1]))
+    for s in samples:
+        diff = s - mean;
+        cov_row = cov_row + np.dot(diff.T, diff)
+    cov_row /= float(len(samples))
+    row_eval, row_evec = np.linalg.eigh(cov_row)
+    # select the top t evals
+    sorted_index = np.argsort(row_eval)
+    # using slice operation to reverse
+    X = row_evec[:,sorted_index[:-row_top-1 : -1]]
+    Z=X
+    return X, Z
     
-    X, Z = PCA2D_2D(x_train, wantaftersize, wantaftersize)
-    x_val_R=np.zeros((len(x_val),wantaftersize,wantaftersize))
-    for sz in range(len(x_val)):
-        x_val_R[sz] = np.dot(Z.T, np.dot(x_val[sz], X))
-
-    x_train_R = x_train_R.reshape((x_train_R.shape[0], x_train_R.shape[1], x_train_R.shape[2], 1))
-    # print(x_train_R.shape)
-    x_val_R = x_val_R.reshape((x_val_R.shape[0], x_val_R.shape[1], x_val_R.shape[2], 1))
+x_train=...   
+X, Z = PCA2D_2D(x_train, wantaftersize, wantaftersize)
+x_train_R=np.zeros((len(x_train),wantaftersize,wantaftersize))
+for sz in range(len(x_train)):
+    x_train_R[sz] = np.dot(Z.T, np.dot(x_train[sz], X))
     
-    x_test_R2=np.zeros((len(XTEST2),wantaftersize,wantaftersize))
-    for sz in range(len(XTEST2)):
-        x_test_R2[sz] = np.dot(Z.T, np.dot(XTEST2[sz], X))
-    x_test_R2 =x_test_R2 .reshape((x_test_R2.shape[0], x_test_R2.shape[1], x_test_R2.shape[2], 1))
+x_val_R=np.zeros((len(x_val),wantaftersize,wantaftersize))
+for sz in range(len(x_val)):
+    x_val_R[sz] = np.dot(Z.T, np.dot(x_val[sz], X))
 
 
+
+#B2DPCA-SPD under LEM
+# LogmTranfer can be found in SPD_Functions.py
+x_train=LogmTranfer(x_train)
+x_val=LogmTranfer(x_val)
+X, Z = PCA2D_2D(x_train, wantaftersize, wantaftersize)
+x_train_R=np.zeros((len(x_train),wantaftersize,wantaftersize))
+for sz in range(len(x_train)):
+    x_train_R[sz] = np.dot(Z.T, np.dot(x_train[sz], X))
+    
+x_val_R=np.zeros((len(x_val),wantaftersize,wantaftersize))
+for sz in range(len(x_val)):
+    x_val_R[sz] = np.dot(Z.T, np.dot(x_val[sz], X))
